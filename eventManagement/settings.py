9d719846ae43359a0a1d80c8d02df31d9707b9b4
Yanis Ahmed <yanisahmed12@gmail.com>
2025-08-25 22:16:15 +0600

    [Done] Postgress integration

--- eventManagement/settings.py
+++ eventManagement/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'eventManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#pip install psycopg2-binary
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event_management',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
