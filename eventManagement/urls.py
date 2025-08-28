f0425c65ab61eff79a5b230765a776a82650629b
Yanis Ahmed <yanisahmed12@gmail.com>
2025-08-28 21:34:05 +0600

    [Done] Django Debugger tool setup

--- eventManagement/urls.py
+++ eventManagement/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
     path('', include('event.urls')),
-]
+] + debug_toolbar_urls()
